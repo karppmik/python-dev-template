7dc5703462eb3ce9921325f03dc24fcc61f3c666
karppmik <mikael.karppinen@icloud.com>
2023-06-29 15:33:12 +0300

    Add correct PYTHONPATH and devcontainer.json

--- {{cookiecutter.project_name}}/.devcontainer/post-create.sh
+++ {{cookiecutter.project_name}}/.devcontainer/post-create.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Get the project name from the cookiecutter template
+project_name="{{cookiecutter.package_name}}"
+
+# Set the PYTHONPATH environment variable
+export PYTHONPATH="/workspaces/${project_name}/src"
+
 poetry lock --no-update
 poetry update
 poetry install
